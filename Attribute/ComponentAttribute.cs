8b94dbb4da5c46d8962e25737633a1820c4bb231
Myron <pk811219@gmail.com>
2021-11-02 11:36:39 +0800

    限制Attribute僅用於Class

--- Attribute/ComponentAttribute.cs
+++ Attribute/ComponentAttribute.cs
@@ -2,14 +2,17 @@
 
 namespace AutoRegister.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class)]
     public class ComponentAttribute : Attribute
     {
     }
 
+    [AttributeUsage(AttributeTargets.Class)]
     public class ServiceAttribute : ComponentAttribute
     {
     }
 
+    [AttributeUsage(AttributeTargets.Class)]
     public class RepositoryAttribute : ComponentAttribute
     {
     }
--- Attribute/LifeTimeAttribute.cs
+++ Attribute/LifeTimeAttribute.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace AutoRegister.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class)]
     public class LifeTimeAttribute : Attribute
     {
         public ServiceLifetime lifetime;
@@ -10,16 +11,19 @@ namespace AutoRegister.Attributes
         public LifeTimeAttribute(ServiceLifetime lifetime = ServiceLifetime.Singleton) => this.lifetime = lifetime;
     }
 
+    [AttributeUsage(AttributeTargets.Class)]
     public class SingletonAttribute : LifeTimeAttribute
     {
         public SingletonAttribute() : base(ServiceLifetime.Singleton) { }
     }
 
+    [AttributeUsage(AttributeTargets.Class)]
     public class ScopedAttribute : LifeTimeAttribute
     {
         public ScopedAttribute() : base(ServiceLifetime.Scoped) { }
     }
 
+    [AttributeUsage(AttributeTargets.Class)]
     public class TransientAttribute : LifeTimeAttribute
     {
         public TransientAttribute() : base(ServiceLifetime.Transient) { }
